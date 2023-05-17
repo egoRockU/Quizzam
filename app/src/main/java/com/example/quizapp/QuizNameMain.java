9ea50b8d519d0820031f02ede524e72eb616010a
Stshiii <vergara.jhude.bscs2021@gmail.com>
2023-05-17 18:28:38 +0800

    Settings Done

--- app/src/main/java/com/example/quizapp/QuizNameMain.java
+++ app/src/main/java/com/example/quizapp/QuizNameMain.java
@@ -1,13 +1,27 @@
 package com.example.quizapp;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.content.res.AssetManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
+import android.widget.TextView;
+
+import com.google.android.material.navigation.NavigationView;
 
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
@@ -17,16 +31,44 @@ import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
 
-public class QuizNameMain extends AppCompatActivity {
+public class QuizNameMain extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+    public DrawerLayout drawerLayout;
+    Menu menu;
+    NavigationView navigationView;
+    androidx.appcompat.widget.Toolbar Toolbar;
+
 
     private static List<Quiz_Name_Constructor> Questions;
     private ListView quizList;
     public static String chosenTopic;
 
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.dashboard_main);
+
+
+        /* For Drawer */
+        drawerLayout = findViewById(R.id.drawerlayoutdb);
+        navigationView = findViewById(R.id.drawerview);
+        Toolbar = findViewById(R.id.toolbar);
+
+        setSupportActionBar(Toolbar);
+
+        navigationView.bringToFront();
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, Toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+
+        navigationView.setNavigationItemSelectedListener(this);
+
+        navigationView.setCheckedItem(R.id.dashboard);
+
+        /* For Drawer */
 
         quizList = findViewById(R.id.quizList);
 
@@ -53,6 +95,34 @@ public class QuizNameMain extends AppCompatActivity {
         });
     }
 
+    @Override
+    public void onBackPressed() {
+            if(drawerLayout.isDrawerOpen(GravityCompat.START)){
+                drawerLayout.closeDrawer(GravityCompat.START);
+                }
+            else {
+                super.onBackPressed();
+                }
+    }
+
+    @SuppressLint("NonConstantResourceId")
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+
+        switch (menuItem.getItemId()) {
+            case R.id.drawerSettings:
+                Intent intent = new Intent(QuizNameMain.this, Settings.class);
+                startActivity(intent);
+                break;
+            default:
+                System.out.println("no one matches");
+                break;
+        }
+        drawerLayout.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+
     private void getQuizzes(){
 
         try {
@@ -73,4 +143,5 @@ public class QuizNameMain extends AppCompatActivity {
             throw new RuntimeException(e);
         }
     }
+
 }
